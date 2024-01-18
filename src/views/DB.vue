b23ac7c5a6fb7bc2a1d94f76dcc79ca8911c761c
Reza Khodadadi <rezakho@gmail.com>
2024-01-18 23:07:12 +0330

    *

--- src/views/DB.vue
+++ src/views/DB.vue
@@ -2,6 +2,8 @@
 import { defineComponent, type PropType } from 'vue';
 import { PriorityQueue } from '@/Dijkstra';
 import { types } from 'sass';
+import { DataSet } from 'vis-data';
+import { Network } from 'vis-network';
 
 
 export default defineComponent({
@@ -12,7 +14,7 @@ export default defineComponent({
     data()
     {
         return {
-            q: 'r1(x),w2(x),r1(y),c1,w2(x),c2,r4(x)',
+            q: 'r1(x),w2(x),r1(y),r2(y),c1,w2(x),w3(x),w4(o),w1(o),c2,r4(x)',
             query: '',
             // transactions: [] as Array<{name: string, commands: Array<string>}>,
             transactions: new Map<string, Array<string>>(),
@@ -80,7 +82,10 @@ export default defineComponent({
                 //
                 // }
                 // this.commands
-                console.log(transactionNames);
+                console.log([...this.transactions]);
+
+
+                this.drawGraph();
             },
             immediate: true
         }
@@ -88,12 +93,138 @@ export default defineComponent({
 
     mounted()
     {
+        this.drawGraph();
     },
 
     methods: {
         test()
         {
             return /^([rw]\d+\([a-z]\)|[ac]\d+)(,([rw]\d+\([a-z]\)|[ac]\d+))*$/i.test(this.q);
+        },
+
+        edgeExists(edges: Array<{from: string, to: string}>, from: string, to: string)
+        {
+            return !!edges.find(e => e.from === from && e.to === to);
+        },
+
+        drawGraph()
+        {
+            const nodes = [];
+            const edges = [];
+
+            for (let transaction of this.transactions)
+            {
+                nodes.push({
+                    id: 't' + transaction[0],
+                    label: 't' + transaction[0]
+                });
+            }
+
+            // add edges
+            for (let transactionA of this.transactions)
+            {
+                for (let transactionB of this.transactions)
+                {
+                    if (transactionA[0] === transactionB[0])
+                    {
+                        continue;
+                    }
+
+                    for (let a = 0; a < transactionA[1].length; a++)
+                    {
+                        for (let b = a; b < transactionB[1].length; b++)
+                        {
+                            const commandA = transactionA[1][a];
+                            const commandB = transactionB[1][b];
+
+                            if (commandA.startsWith('a') || commandA.startsWith('c') || commandB.startsWith('a') || commandB.startsWith('c'))
+                            {
+                                continue;
+                            }
+
+                            const dataA = transactionA[1][a].substring(transactionA[1][a].indexOf('(') + 1, transactionA[1][a].indexOf(')'));
+                            const dataB = transactionB[1][b].substring(transactionB[1][b].indexOf('(') + 1, transactionB[1][b].indexOf(')'));
+
+                            console.log(dataA, '-', dataB);
+
+                            if ((dataA === dataB) && commandA.startsWith('r') && commandB.startsWith('w') && !this.edgeExists(edges, 't' + transactionA[0], 't' + transactionB[0]))
+                            {
+                                edges.push({
+                                    from: 't' + transactionA[0],
+                                    to: 't' + transactionB[0]
+                                });
+
+                                // console.log(commandA, '-', commandB);
+                            }
+
+                            if ((dataA === dataB) && commandA.startsWith('w') && (commandB.startsWith('r') || commandB.startsWith('w')) && !this.edgeExists(edges, 't' + transactionA[0], 't' + transactionB[0]))
+                            {
+                                edges.push({
+                                    from: 't' + transactionA[0],
+                                    to: 't' + transactionB[0]
+                                });
+
+                                // console.log(commandA, '-', commandB);
+                            }
+
+                            // console.log(commandA, '-', commandB);
+                        }
+                        // if (edges.find(e => e.from ===))
+                    }
+                }
+            }
+
+            console.log(edges);
+
+            var visNodes = new DataSet(nodes);
+
+            // create an array with edges
+            // @ts-ignore
+            var visEdges = new DataSet(edges);
+
+            // create a network
+            var container = document.getElementById('mynetwork');
+            var data = {
+                nodes: nodes,
+                edges: edges
+            };
+            var options = {
+                height: '800',
+
+                interaction: {
+                    zoomView: false
+                },
+
+                edges: {
+                    arrows: {
+                        to: {
+                            enabled: true
+                        }
+                    },
+                    color: {
+                        color: '#000000'
+                    },
+                    width: 2,
+                    length: 250,
+                    physics: true
+                },
+
+                nodes: {
+                    heightConstraint: 50,
+                    shape: 'circle',
+                    physics: false,
+                    font: {
+                        face: 'monospace',
+                        size: 17,
+                        align: 'center'
+                    }
+                }
+            };
+
+            if (this.$refs.graph)
+            {
+                var network = new Network(this.$refs.graph as HTMLElement, data, options);
+            }
         }
     }
 });
@@ -114,9 +245,14 @@ export default defineComponent({
         <table class="table">
             <tr v-for="transaction of transactions">
                 <td>T<sub>{{ transaction[0] }}</sub></td>
-                <td v-for="commands of transaction[1]">{{ commands }}</td>
+                <td v-for="command of transaction[1]">{{ command }}</td>
             </tr>
         </table>
+
+        <div class="graph">
+            <div class="note">گره ها قابل جابجایی هستند</div>
+            <div class="canvas" ref="graph"></div>
+        </div>
     </div>
 </template>
 
@@ -128,7 +264,7 @@ export default defineComponent({
     .q
     {
         text-align: center;
-        margin-bottom: 150px;
+        margin-bottom: 80px;
 
         input
         {
@@ -145,7 +281,7 @@ export default defineComponent({
             &.error
             {
                 border-color: orangered;
-                background-color: #f6d3c6;
+                background-color: #F6D3C6;
             }
         }
     }
@@ -156,6 +292,7 @@ export default defineComponent({
         font-family: monospace;
         font-size: 17px;
         text-transform: lowercase;
+        margin-bottom: 80px;
 
         tr
         {
@@ -185,6 +322,25 @@ export default defineComponent({
                     border-bottom: 0;
                 }
             }
+        }
+    }
+
+    .graph
+    {
+        border: 2px solid #CBCBCB;
+        border-radius: 20px;
+
+        .note
+        {
+            text-align: center;
+            direction: rtl;
+            padding: 10px;
+            user-select: none;
+        }
+
+        .canvas
+        {
+            height: 100%;
         }
     }
 }
